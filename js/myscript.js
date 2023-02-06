3898fc860b74b11686405d8ff2dc74ba4b98e66a
Timilehin Ibigbami <122795579+Teekay40@users.noreply.github.com>
2023-02-06 17:05:12 +0100

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,21 @@
+
+function redirect(){
+    const url = "http://resume.io/r/mcPXzfOlP";
+    window.location.href= "http://resume.io/r/mcPXzfOlP";
+}
+
+
+function myFunction() {
+  alert("Your message has been sent successfully !");
+  window.location.reload();
+}  
+
+
+function myFunction() {
+  const x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
